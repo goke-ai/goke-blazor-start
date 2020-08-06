613735aa5cc4d412f17e34cfedf5b9cbcd1f1b6f
Goke <gokeladokun@gmail.com>
2020-08-06 15:45:18 +0800

    Add Blazor Webassembly project

---
+++ src/Goke.BSA/Client/Services/Contracts/IAuthorizeApi.cs
@@ -0,0 +1,17 @@
+﻿using Goke.Authentication;
+using Goke.BSA.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goke.BSA.Client.Services.Contracts
+{
+    public interface IAuthorizeApi
+    {
+        Task Login(LoginParameters loginParameters);
+        Task Register(RegisterParameters registerParameters);
+        Task Logout();
+        Task<UserInfo> GetUserInfo();
+    }
+}
---
+++ src/Goke.BSA/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Goke.BSA.Server.Models;
+using Goke.BSA.Shared;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goke.BSA.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
